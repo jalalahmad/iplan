f91e848e3e72b9c5b5e4f16fbee736760423364a
Jalal Ahmad <jalal.ahmad@gmail.com>
2021-02-14 12:38:02 -0500

    Initiialize directory with basic config file

--- cmd/init.go
+++ cmd/init.go
@@ -1,12 +1,24 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+	"io/ioutil"
+)
 
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "initialize directory for iplan use",
-	Long: `Sets up config file with sample story and make directory versionable.`,
+	Long:  `Sets up config file with sample story and make directory versionable.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// Do Stuff Here
+		createConfigFile()
 	},
+}
+
+func createConfigFile() {
+	d1 := []byte(`---
+author: YOUR NAME <you@email.me>
+`)
+	err := ioutil.WriteFile("./.iplan.yaml", d1, 0644)
+	cobra.CheckErr(err)
 }
